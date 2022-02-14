b4f1074e1e8d2d1047db8bd4098a8ed84d4e2ca9
Jooa <code@jooa.xyz>
2022-02-14 13:28:13 +0100

    Export module functions

--- src/ModellingFramework.jl
+++ src/ModellingFramework.jl
@@ -13,4 +13,40 @@ include("ode_model.jl")
 include("model_objective.jl")
 include("model_fit.jl")
 
+# Types
+export ModelData,
+    ModelFit,
+    ModelObjective,
+    ODEModel,
+    # Helpers
+    parameter_array,
+    parameter_dict,
+    parameter_index,
+    # Model methods
+    initial,
+    ratefun,
+    bounds,
+    lower_bound,
+    upper_bound,
+    link,
+    simulate,
+    starts,
+    output_names,
+    link_names,
+    child,
+    model,
+    # Data methods
+    weights,
+    times,
+    fields,
+    vals,
+    residuals,
+    sample_size,
+    free_parameter_count,
+    AIC,
+    # Maths
+    exponential_decay,
+    flattening_curve,
+    hill
+
 end # module
